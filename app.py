fda69556904482ce094098946fbfd6ad4664623c
snehatandon98 <snehatandon.98@gmail.com>
2022-05-19 17:01:56 +0530

    MINI ASSIGNMENT

--- app.py
+++ app.py
@@ -1,26 +1,109 @@
-from crypt import methods
-from urllib import response
-from flask import Flask, request
 import movie_service as dynamodb
+import jwt
+import datetime
+import config
+import time
+import logging
+from jwt import decode
+from functools import wraps
+from apscheduler.schedulers.background import BackgroundScheduler
+from crypt import methods
+from datetime import datetime
+from urllib import response
+from flask import Flask, request,jsonify, Response, make_response
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY']=config.SECRET_KEY
+scheduler = BackgroundScheduler()
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        print(app.config['SECRET_KEY'])
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
 
+        if not token:
+            return jsonify({'message' : 'Token is missing!'}), 401
+
+        try: 
+            data = jwt.decode(token, app.config['SECRET_KEY'],algorithms=["HS256"])
+        except:
+            return jsonify({'message' : 'Token is invalid!'}), 401
+
+        return f(*args, **kwargs)
+
+    return decorated
+
+@app.route('/createTable')
+def root_route():
+    dynamodb.create_table_movies()
+    return 'Table created'
+
+def sync():
+    dynamodb.populate_table()
+
+
+#Getting list of Movie Titles directed by given director in the given year range
 @app.route('/titles/<director>/<int:year_start>/<int:year_end>',methods=['GET'])
+@token_required
 def generate_titles_report(director,year_start,year_end):
-    response=dynamodb.generate_titles_report(director,year_start,year_end)
+    start=time.time()
+    try:
+        response=dynamodb.generate_titles_report(director,year_start,year_end)
+        
+    except:
+        response= jsonify({'message' : 'Invalid request'})
+    end=time.time()
+    total=int(round(1000*(end-start)))
+    response.headers["X-TIME-TO-EXECUTE"]=total
     return response
 
+
+#Getting list of English Movie titles which have user reviews more than given user review 
 @app.route('/titles/<int:user_review>',methods=['GET'])
+@token_required
 def generate_english_titles(user_review):
-    response=dynamodb.generate_english_titles(user_review)
+    start = time.time()
+    try:
+        response=dynamodb.generate_english_titles(user_review)
+    except:
+        response= jsonify({'message' : 'Invalid request'})
+    end=time.time()
+    total=int(round(1000*(end-start)))
+    response.headers["X-TIME-TO-EXECUTE"]=total
     return response
 
+
+#Getting the highest budget title for the given year and country.
 @app.route('/titles/<country>/<int:year>',methods=['GET'])
+@token_required
 def generate_highest_budget_titles(country,year):
-    response=dynamodb.generate_highest_budget_titles(country,year)
+    start=time.time()
+    try:
+        response=dynamodb.generate_highest_budget_titles(country,year)
+    except:
+        response= jsonify({'message' : 'Invalid request'})
+    end=time.time()
+    total=int(round(1000*(end-start)))
+    response.headers["X-TIME-TO-EXECUTE"]=total
     return response
 
+
+#Login for authentication to get the "X-ACCESS-TOKEN".
+@app.route('/login')
+def login():
+    auth=request.authorization
+    if auth and auth.password=='password':
+        token=jwt.encode({'user':auth.username}, app.config['SECRET_KEY'])
+        return jsonify({'token' : token})
+    return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    #Background Scheduler to sync the data every 60 minutes.
+    job = scheduler.add_job(sync, 'interval', minutes=60)
+    scheduler.start()
+
+    app.run(host='127.0.0.1', port=5000, debug=True, use_reloader=False)
--- movie_service.py
+++ movie_service.py
@@ -1,16 +1,24 @@
+from email import message
+from tkinter.tix import COLUMN
+import config
+import boto3
+import csv
+import logging
+from datetime import datetime
+from time import time
 from urllib import response
 from boto3 import resource
-import boto3
 from boto3.dynamodb.conditions import Key, Attr
-import csv
-
 from flask import jsonify
-import config
+
+#Setting the AWS Credentials
 
 AWS_ACCESS_KEY_ID = config.AWS_ACCESS_KEY_ID
 AWS_SECRET_ACCESS_KEY = config.AWS_SECRET_ACCESS_KEY
 REGION_NAME = config.REGION_NAME
 ENDPOINT_URL = config.ENDPOINT_URL
+
+#Importing DynamoDb as a resource
 
 resource = resource(
     'dynamodb',
@@ -19,20 +27,19 @@ resource = resource(
     aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
     region_name           = REGION_NAME
 )
-def main():
 
-    #create_table_movies()
+logging.basicConfig(level=logging.INFO)
+
+#Reading the CSV File and passing the data to "write_to_dynamo" function
+def populate_table():
     csvfile = open('Movie.csv')
     header = []
     header = next(csvfile)
     write_to_dynamo(csv.reader(csvfile,delimiter=",",quotechar='"'))
-    print("Done")
-    generate_titles_report("D.W. Griffith",1900,2010)
-    print("-----------------------------------------------------")
-    generate_english_titles(9)
-    print("-------------------------------------------------------------")
-    generate_highest_budget_titles('France',1921)
+    t=datetime.now()
+    logging.info("File Synced at " + str(t))
 
+#Creating Movies Table
 def create_table_movies():   
    table = resource.create_table(
        TableName = 'Movies', # Name of the table
@@ -61,82 +68,103 @@ def create_table_movies():
            'WriteCapacityUnits': 10
        }
    )
+   logging.info("TABLE CREATED")
    return table
 MovieTable = resource.Table('Movies')
 
+#Inserting data from CSV File to Movies Table
 def write_to_dynamo(rows):
+    logging.info("Writing to DynamoDb started.")
     count = 0
-    for row in rows:
+    for col in rows:
         number=''
         count = count + 1
-        if row[20]=='':
-            row[20]=0
-        if row[16]=='':
-            row[16]=0
+        if col[20]=='':
+            col[20]=0
+        if col[16]=='':
+            col[16]=0
         else:
-            for i in row[16]:
+            for i in col[16]:
                 if i.isdigit():
                     number+=i
-            row[16]=int(number)
+            col[16]=int(number)
         response = MovieTable.put_item(
                 Item={
-                    'imdb_id' : row[0],
-                    'title': row[1],
-                    'original_title':row[2],
-                    'year':int(row[3]),
-                    'date_published':row[4],
-                    'genre':row[5],
-                    'duration':row[6],
-                    'country':row[7],
-                    'language':row[8],
-                    'director':row[9],
-                    'writer':row[10],
-                    'production_company':row[11],
-                    'actors':row[12],
-                    'description':row[13],
-                    'avg_vote':row[14],
-                    'votes':row[15],
-                    'budget':row[16],
-                    'usa_gross_income':row[17],
-                    'worlwide_gross_income':row[18],
-                    'metascore':row[19],
-                    'reviews_from_users':int(row[20]),
-                    'reviews_from_critics':row[21],
+                    'imdb_id' : col[0],
+                    'title': col[1],
+                    'original_title':col[2],
+                    'year':int(col[3]),
+                    'date_published':col[4],
+                    'genre':col[5],
+                    'duration':col[6],
+                    'country':col[7],
+                    'language':col[8],
+                    'director':col[9],
+                    'writer':col[10],
+                    'production_company':col[11],
+                    'actors':col[12],
+                    'description':col[13],
+                    'avg_vote':col[14],
+                    'votes':col[15],
+                    'budget':col[16],
+                    'usa_gross_income':col[17],
+                    'worlwide_gross_income':col[18],
+                    'metascore':col[19],
+                    'reviews_from_users':int(col[20]),
+                    'reviews_from_critics':col[21],
                     
                 }
             )
+    logging.info("Writing to DynamoDb finished. Data succefully inserted into the Table.")
+    
 
+#Getting list of Movie Titles directed by given director 
+# -in the given year range from the Database
 def generate_titles_report(director,year_start,year_end):
         response=MovieTable.scan(
             FilterExpression=Attr('director').eq(director) & Attr('year').between(year_start,year_end)
             )
         titles=response['Items']
-        result=[]
-        for t in titles:
-            result.append(t['title'])
-        return jsonify(result)
+        if len(titles) ==0:
+            return jsonify({'message': 'No records found for the request.'})
+        else:
+            result=[]
+            for t in titles:
+                result.append(t['title'])
+            return jsonify(result)
 
+#Getting list of English Movie titles which have user reviews more 
+#than given user review from the database in descending order.
 def generate_english_titles(user_review):
     response=MovieTable.scan(
         FilterExpression=Attr('language').eq('English')  & Attr('reviews_from_users').gte(user_review)
     )
     titles=response['Items']
+    res=[]
     result=[]
-    for t in titles:
-        result.append(t['title'])
-    return jsonify(result)
+    if len(titles)==0:
+        return jsonify({'message': 'No records found for the request.'})
+    else:
+        for t in titles:
+            res.append((t['reviews_from_users'],t['title']))
+        res.sort(key = lambda x: x[0],reverse=True)
+        result = [x[1] for x in res]
+        return jsonify(result)
+
+
+#Getting the highest budget title for the given year and country from the database.
 def generate_highest_budget_titles(country,year):
     response=MovieTable.scan(
         FilterExpression=Attr('country').contains(country) & Attr('year').eq(year)
     )   
     titles=response['Items']
     b=0
-    title=''
-    #print(titles)
-    for t in titles:
-        if t['budget']>b:
-            title=t['title']
-            b=t['budget']
-    return jsonify(title)
-if __name__ == '__main__':
-    main()
+    if len(titles)==0:
+        return jsonify({'message': 'No records found for the request.'})
+    else:
+        title=''
+        for t in titles:
+            if t['budget']>b:
+                title=t['title']
+                b=t['budget']
+        return jsonify(title)
